20ecd0c2de0643b570af44959a5943b1936acff9
Ekaterina <katrine.d@gmail.com>
2020-09-22 17:08:40 +0300

    Initial Commit

---
+++ VC_new/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VC_new
+//
+//  Created by Ekaterina Donskaya on 22/09/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
